f9644d53e9bfb09c794aa389d50b5e351490d815
dheeraj7873 <99673230+dheeraj7873@users.noreply.github.com>
2022-09-04 21:35:33 -0700

    olympic rings from python code

---
+++ Untitled11.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle
+
+turtle.color("blue")
+turtle.penup()
+turtle.goto(-110, -25)
+turtle.pendown()
+turtle.circle(45)
+
+turtle.color("black")
+turtle.penup()
+turtle.goto(0, -25)
+turtle.pendown()
+turtle.circle(45)
+
+turtle.color("red")
+turtle.penup()
+turtle.goto(110, -25)
+turtle.pendown()
+turtle.circle(45)
+
+turtle.color("yellow")
+turtle.penup()
+turtle.goto(-55, -75)
+turtle.pendown()
+turtle.circle(45)
+
+turtle.color("green")
+turtle.penup()
+turtle.goto(55, -75)
+turtle.pendown()
+turtle.circle(45)
+
+turtle.done()
+
+
+# In[ ]:
+
+
+
+
